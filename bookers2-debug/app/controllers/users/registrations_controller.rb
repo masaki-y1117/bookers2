3f5616968fb6919ca714378d2ffd1c0a3d18273e
masaki-y1117 <my1117yamamoto@gmail.com>
2021-05-22 04:52:48 +0000

    Fix ActionMailer

--- bookers2-debug/app/controllers/users/registrations_controller.rb
+++ bookers2-debug/app/controllers/users/registrations_controller.rb
@@ -13,11 +13,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.with(user: @user).welcome_mail.deliver_later
+      ContactMailer.with(user: @user).welcome_email.deliver_later
     end
-    logger.debug @user.errors.inspect 
-    # redirect_to user_path(@user)
   end
+  
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
 
   # GET /resource/edit
   # def edit
@@ -66,7 +69,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   private
+  
   def user_params
-    params.require(:user).permit(:email, :name)
+    params.require(:user).permit(:email, :name, :password)
   end
 end
